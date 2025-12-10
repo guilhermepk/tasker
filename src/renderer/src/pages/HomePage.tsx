331992f0e7a89c4d634a3415d56e2aedeb8c89b8
Guilherme Prigol Kramer <guilhermepk.pormade@gmail.com>
2025-12-10 15:03:46 -0300

    roteamento de paginas

---
+++ src/renderer/src/pages/HomePage.tsx
@@ -0,0 +1,43 @@
+import { createSignal, type Component } from 'solid-js'
+import Versions from '../components/Versions'
+import electronLogo from '../assets/electron.svg'
+
+const HomePage: Component = () => {
+  const [count, setCount] = createSignal<number>(0);
+
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <img alt="logo" class="logo" src={electronLogo} />
+      <div class="creator">Powered by electron-vite</div>
+
+      <div class="text">
+        Build an Electron app with <span class="solid">Solid</span>
+        &nbsp;and <span class="ts">TypeScript</span>
+      </div>
+
+      <p class="tip">
+        O botão foi clicado <code>{count()}</code> vezes.
+      </p>
+
+      <div class="actions">
+        <div class="action">
+          <a onClick={() => setCount(prev => prev + 1)}>
+            Contar
+          </a>
+        </div>
+
+        <div class="action">
+          <a onClick={ipcHandle}>
+            Send IPC
+          </a>
+        </div>
+      </div>
+
+      <Versions />
+    </>
+  )
+}
+
+export default HomePage;
