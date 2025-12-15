09474033ccba6a3b1d6836a0bbd6aa55c48eac50
Guilherme Prigol Kramer <guilhermepk.pormade@gmail.com>
2025-12-15 08:28:54 -0300

    ajustes

--- src/renderer/src/pages/HomePage.tsx
+++ src/renderer/src/pages/HomePage.tsx
@@ -1,48 +1,10 @@
-import { createSignal, type Component } from 'solid-js'
-import Versions from '../components/Versions'
-import electronLogo from '../assets/electron.svg'
-import Teste from '@renderer/components/Teste';
+import { type Component } from 'solid-js';
 
 const HomePage: Component = () => {
-  const [count, setCount] = createSignal<number>(0);
-
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
-    <>
-      <img alt="logo" class="logo" src={electronLogo} />
-      <div class="creator">Powered by electron-vite</div>
-
-      <div class="text">
-        Build an Electron app with <span class="solid">Solid</span>
-        &nbsp;and <span class="ts">TypeScript</span>
-      </div>
-
-      <p class="tip">
-        O botão foi clicado <code>{count()}</code> vezes.
-      </p>
-
-      <Teste
-        text='Bom dia'
-        mainColor='#00918d'
-      />
-
-      <div class="actions">
-        <div class="action">
-          <a onClick={() => setCount(prev => prev + 1)}>
-            Contar
-          </a>
-        </div>
-
-        <div class="action">
-          <a onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-
-      <Versions />
-    </>
+    <div>
+      Bom dia
+    </div>
   )
 }
 
