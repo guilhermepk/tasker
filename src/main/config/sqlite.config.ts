27d963a69c159c1f136fd7b2ee16ce1e4fc143c0
Guilherme Prigol Kramer <guilhermepk.pormade@gmail.com>
2026-01-30 07:50:34 -0300

    Update sqlite.config.ts

--- src/main/config/sqlite.config.ts
+++ src/main/config/sqlite.config.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { app } from 'electron';
+import path from 'path';
 
 @Injectable()
 export class TypeOrmConfigService {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'sqlite',
-      database: 'tasker.db',
+      database: path.join(app.getPath('userData'), 'tasker.db'),
       synchronize: true,
       autoLoadEntities: true,
     };
