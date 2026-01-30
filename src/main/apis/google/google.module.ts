e0f36142148dca3cdbf21da299d7550df125ecda
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2026-01-30 16:58:32 -0300

    credenciais do Google setadas na inicialização do app

--- src/main/apis/google/google.module.ts
+++ src/main/apis/google/google.module.ts
@@ -1,10 +1,13 @@
-import { Module } from "@nestjs/common";
+import { Inject, Logger, Module, OnApplicationBootstrap } from "@nestjs/common";
 import { Auth } from "googleapis";
 import { StartGoogleAuthUseCase } from "./use-cases/start-auth/start-google-auth.use-case";
 import * as dotenv from 'dotenv';
 import { SecureDataManagerModule } from "@main/secure-data-manager/secure-data-manager.module";
 import { IsGoogleAuthenticatedUseCase } from "./use-cases/is-authenticated/is-google-authenticated.use-case";
 import { LogoutGoogleUseCase } from "./use-cases/logout-google/logout-google.use-case";
+import { ReadGoogleTokenUseCase } from "@main/secure-data-manager/use-cases/read-google-token/read-google-token.use-case";
+import { tryCatch } from "@main/common/utils/try-catch";
+import { GoogleKeys } from "@main/secure-data-manager/models/types/google-keys.type";
 
 dotenv.config();
 
@@ -28,4 +31,35 @@ dotenv.config();
     LogoutGoogleUseCase
   ]
 })
-export class GoogleModule {}
+export class GoogleModule implements OnApplicationBootstrap {
+  logger: Logger
+
+  constructor(
+    @Inject(Auth.OAuth2Client)
+    private readonly oAuth2Client: Auth.OAuth2Client,
+
+    @Inject(ReadGoogleTokenUseCase)
+    private readonly readGoogleTokenUseCase: ReadGoogleTokenUseCase
+  ){
+    this.logger = new Logger(GoogleModule.name);
+  }
+
+  async onApplicationBootstrap() {
+    await tryCatch(async () => {
+      const googleKeys: GoogleKeys | null = await this.readGoogleTokenUseCase.execute();
+
+      if (googleKeys) {
+        this.oAuth2Client.setCredentials({
+          access_token: googleKeys.accessToken,
+          refresh_token: googleKeys.refreshToken
+        });
+        this.logger.log('Google autenticado');
+      } else {
+        this.logger.log('Google não autenticado');
+      }
+    }, `Erro ao inicializar credenciais do Google`)
+      .catch((error) => {
+        this.logger.error(error);
+      });
+  }
+}
